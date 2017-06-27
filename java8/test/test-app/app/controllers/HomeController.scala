519da9c83e2a6ab0a12645867ebe55a2af10e2a7
PeuterBart <Bartjodp@hotmail.com>
2017-06-27 15:24:53 +0200

    Update HomeController.scala

--- java8/test/test-app/app/controllers/HomeController.scala
+++ java8/test/test-app/app/controllers/HomeController.scala
@@ -18,7 +18,7 @@ class HomeController @Inject() extends Controller {
    * a path of `/`.
    */
   def index = Action {
-    Ok(views.html.index("Hi BBlocks! Your new scala mvc application is ready. Bart"))
+    Ok(views.html.index("Hi BBlocks! Your new scala mvc application is ready. Bart, nu via webhook"))
   }
 
 }
